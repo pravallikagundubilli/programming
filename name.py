e62ec231ab6d77735662bbea8af88c17e6219f5f
pravallika gundubilli <84492238+pravallikagundubilli@users.noreply.github.com>
2021-06-01 22:16:40 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+s=input("enter your name")
+n=int(input("how many times u want to print your name:"))
+if n>0:
+    print(s*n,end="\n")
+else:
+     print("printing your name is not possible",end="\n")
+     print("please try with a valid input")
+   
