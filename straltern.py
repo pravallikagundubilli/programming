682e111583c949b2d4f3770063ced5eb82d986d3
pravallika gundubilli <84492238+pravallikagundubilli@users.noreply.github.com>
2021-06-03 19:45:33 +0530

    Add files via upload

---
+++ straltern.py
@@ -0,0 +1,9 @@
+str1=input("enter string 1")
+str2=input("enter string 2 of same length of 1")
+a=len(str1);b=len(str2)
+if a!=b:
+    print("the length of two strings are not same")
+else:
+    for i in range(a):
+        print("{}{}".format(str1[i],str2[i]),end="")
+
