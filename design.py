3ee27f009e1d6f015a130b2185d6ef733e24432b
Krishna Pravallika <84492238+pravallikagundubilli@users.noreply.github.com>
2021-11-21 17:58:34 +0530

    Add files via upload

---
+++ design.py
@@ -0,0 +1,44 @@
+##turtle.done()
+
+##
+##import turtle
+##from turtle import *
+##
+##turtle.title("rainbow spiral")
+##speed(0)
+##bgcolor("black")
+##r,g,b=255,0,0
+##
+##for i in range (255*2):
+##    colormode(255)
+##    if i <255/3:
+##        g+=3
+##    elif i<255*2//3:
+##        r-=3
+##    elif i<255:
+##        b+=3
+##    elif i<255*4//3:
+##        g-=3
+##    elif i<255*5//3:
+##        r+=3
+##    else:
+##        b-=3
+##    fd(50+i)
+##    rt(91)
+##    pencolor(r,g,b)
+##turtle.done()
+
+
+import turtle
+t = turtle.Turtle()
+s= turtle.Screen()
+
+s.bgcolor('black')
+t.width(2)
+t.speed(15)
+col=('yellow','green','red','blue')
+
+for i in range(300):
+    t.pencolor(col[i%4])
+    t.forward(i*4)
+    t.right(137)
---
+++ sym.py
@@ -0,0 +1,18 @@
+from turtle import *
+color('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("sky blue")
+pen = turtle. Turtle()
+pen. write("Many Happy Returns Of The Day Dad", font=("algerian",40, "italic"),align="center")
